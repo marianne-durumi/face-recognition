bd48182fc3043427bed6259cc1593c03062270a5
durumi <durams@naver.com>
2020-12-29 22:25:33 +0900

    preparing Clarifai

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+/* eslint-disable no-console */
+/* eslint-disable react/no-unused-state */
+import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
@@ -25,17 +28,38 @@ const particlesOptions = {
   },
 };
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className="particles" params={particlesOptions} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+const app = new Clarifai.App({
+  apiKey: '5cc310e469f5442a948f53890ee1c93e',
+});
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log('click');
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm inputChange={this.onInputChange} buttonSubmit={this.onButtonSubmit} />
+        {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,13 +1,18 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => (
+const ImageLinkForm = ({ inputChange, buttonSubmit }) => (
   <>
     <p className="f3">This Magic Brain will detect faces in your pictures. Git it a try</p>
     <div className="flex justify-center">
       <div className="flex justify-center pa4 br3 shadow-5 form">
-        <input className="w-70 f4 pa2" type="text" />
-        <button className="w-30 grow f4 link ph3 pv2 white bg-light-purple" type="submit">
+        <input className="w-70 f4 pa2" type="text" onChange={inputChange} />
+        <button
+          className="w-30 grow f4 link ph3 pv2 white bg-light-purple"
+          type="submit"
+          onClick={buttonSubmit}
+        >
           Detect
         </button>
       </div>
